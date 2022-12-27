1f53bd32c7370b967b4d0384b5e747e1470b7b07
leshukav <sc-service@bk.ru>
2022-12-27 15:54:24 +0300

    modV2 netWork1

--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -6,8 +6,9 @@ data class Post(
     val content: String,
     val published: Long,
     val likes: Int = 0,
-    val share: Int = 990,
-    val viewEye: Int = 1,
     val likedByMe: Boolean,
+) {
+    val share: Int = 990
+    val viewEye: Int = 1
     val video: String = "https://www.youtube.com/watch?v=vJ8unmdwT3M"
-)
+}
